c2642aa7dbebafe71918a2666e5432e0f59dcf6f
Pedro Augusto <pedroamfarias@gmail.com>
2019-04-22 00:03:03 -0300

    fix indentation

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -48,7 +48,7 @@ def handle_calculate_IK(req):
                     [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
                     [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
                     [                 0,                 0,           0,             1]])
-    return TF
+        return TF
 	# Create individual transformation matrices
 	#
     T0_1 = TF_Mat(alpha0, a0, d1, q1).subs(dh)
@@ -87,82 +87,82 @@ def handle_calculate_IK(req):
         ###
 
         # Initialize service response
-        joint_trajectory_list = []
-        for x in xrange(0, len(req.poses)):
-            # IK code starts here
-            joint_trajectory_point = JointTrajectoryPoint()
+    joint_trajectory_list = []
+    for x in xrange(0, len(req.poses)):
+        # IK code starts here
+        joint_trajectory_point = JointTrajectoryPoint()
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
-            px = req.poses[x].position.x
-            py = req.poses[x].position.y
-            pz = req.poses[x].position.z
+    # Extract end-effector position and orientation from request
+    # px,py,pz = end-effector position
+    # roll, pitch, yaw = end-effector orientation
+        px = req.poses[x].position.x
+        py = req.poses[x].position.y
+        pz = req.poses[x].position.z
 
-            (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
-                [req.poses[x].orientation.x, req.poses[x].orientation.y,
-                    req.poses[x].orientation.z, req.poses[x].orientation.w])
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(
+            [req.poses[x].orientation.x, req.poses[x].orientation.y,
+                req.poses[x].orientation.z, req.poses[x].orientation.w])
 
-            ### Your IK code here
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-            # Rotation Matrices:
-            r, p, y = symbols('r p y')
+        ### Your IK code here
+    # Compensate for rotation discrepancy between DH parameters and Gazebo
+    #
+        # Rotation Matrices:
+        r, p, y = symbols('r p y')
 
-            ROT_x = Matrix([[     1,       0,       0],
-                        [       0,  cos(r), -sin(r)],
-                        [       0,  sin(r),  cos(r) ]])
+        ROT_x = Matrix([[     1,       0,       0],
+                    [       0,  cos(r), -sin(r)],
+                    [       0,  sin(r),  cos(r) ]])
 
-            ROT_y = Matrix([[cos(p),       0,  sin(p)],
-                        [       0,       1,       0 ],
-                        [ -sin(p),       0,  cos(p) ]])
+        ROT_y = Matrix([[cos(p),       0,  sin(p)],
+                    [       0,       1,       0 ],
+                    [ -sin(p),       0,  cos(p) ]])
 
-            ROT_Z = Matrix([[ cos(y), -sin(y),       0],
-                        [   sin(y),  cos(y),       0 ],
-                        [        0,       0,       1 ]])                  
+        ROT_Z = Matrix([[ cos(y), -sin(y),       0],
+                    [   sin(y),  cos(y),       0 ],
+                    [        0,       0,       1 ]])                  
 
-            ROT_EE = ROT_z * ROT_y * ROT_x
+        ROT_EE = ROT_z * ROT_y * ROT_x
 
-            # Apply correct to EE
+        # Apply correct to EE
 
-            Rot_Error = ROT_Z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+        Rot_Error = ROT_Z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
 
-            ROT_EE = ROT_EE * Rot_Error
-            ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
+        ROT_EE = ROT_EE * Rot_Error
+        ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
-            EE = Matrix([[px],
-                        [py],
-                        [pz]])
-            WC = EE - (0.303) * ROT_EE[:,2]
-	    # Calculate joint angles using Geometric IK method
-	    #
-            theta1 = atan2(WC[1],WC[0])
+        EE = Matrix([[px],
+                    [py],
+                    [pz]])
+        WC = EE - (0.303) * ROT_EE[:,2]
+    # Calculate joint angles using Geometric IK method
+    #
+        theta1 = atan2(WC[1],WC[0])
 
-            side_a = 1.501
-            side_b = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
-            side_c = 1.25
+        side_a = 1.501
+        side_b = sqrt(pow((sqrt(WC[0]*WC[0] + WC[1]*WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
+        side_c = 1.25
 
-            angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
-            angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
-            angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
+        angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+        angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+        angle_c = acos((side_a * side_a + side_b * side_b - side_c * side_c) / (2 * side_a * side_b))
 
-            theta2 = pi/2 - a - atan2(WC[2]-0.75, sqrt(WC[0]*WC[0]+WC[1]*WC[1])-0.35)
-            theta3 = pi/2 - (b+0.036)
+        theta2 = pi/2 - a - atan2(WC[2]-0.75, sqrt(WC[0]*WC[0]+WC[1]*WC[1])-0.35)
+        theta3 = pi/2 - (b+0.036)
 
-            R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
-            R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3:theta3})
+        R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+        R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3:theta3})
 
-            R3_6 = R0_3.transpose() * ROT_EE
+        R3_6 = R0_3.transpose() * ROT_EE
 
-            theta4 = atan2(R3_6[2,2], -R3_6[0,2])
-            theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
-            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+        theta4 = atan2(R3_6[2,2], -R3_6[0,2])
+        theta5 = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
+        theta6 = atan2(-R3_6[1,1], R3_6[1,0])
 
 
-            ###
+        ###
 
-            # Populate response for the IK request
-            # In the next line replace theta1,theta2...,theta6 by your joint angle variables
+        # Populate response for the IK request
+        # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
 	    joint_trajectory_list.append(joint_trajectory_point)
 
